1ed3d29ba052152ac6f3b378fbe3c7813837515d
Ramiro Castillo <rcastilloc@falabella.cl>
2023-02-25 01:16:46 -0300

    Added gke infra tf and postgresql helm files

--- postgresql/deployment/dev.tfvars
+++ postgresql/deployment/dev.tfvars
@@ -3,3 +3,11 @@ env = "dev"
 cluster_name = "flash-app-v1-dev"
 
 location = "europe-west1"
+
+kubeconfig_path = "~/.kube/config"
+
+pgDb = "pgdb"
+
+pgPass = "admin"
+
+pgUser = "admin"
--- postgresql/deployment/main.tf
+++ postgresql/deployment/main.tf
@@ -1,30 +1,8 @@
 # Deploy pgsql cluster on GKE
-
-# https://registry.terraform.io/providers/hashicorp/helm/latest/docs
-provider "helm" {
-  kubernetes {
-    config_path = var.kubeconfig_path
-  }
-  alias = "gke"
-}
-
-# https://www.terraform.io/docs/language/settings/index.html
-terraform {
-  required_version = ">= 1.0.0"
-  required_providers {
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.3.0"
-    }
-  }
-}
-
 module "postgresql" {
-  source = "../modules"
-
-  enable_ha          = var.env == "prod" ? true : false
-  postgresqlPassword = var.pgPass
-  postgresqlUsername = var.pgUser
-  postgresqlDatabase = var.pgDb
-
+  source    = "../modules"
+  env       = var.env
+  pgPass    = var.pgPass
+  pgUser    = var.pgUser
+  pgDb      = var.pgDb
 }
