36f27c1a6ad64ad9432db926e93e14128d736b80
Ramiro Castillo <rcastilloc@falabella.cl>
2023-03-05 17:45:51 -0300

    changed postegresql deployment

--- postgresql/modules/main.tf
+++ postgresql/modules/main.tf
@@ -1,25 +1,31 @@
-// PGSQL DEV
+# PGSQL DEV
+# Chart documentation: https://artifacthub.io/packages/helm/bitnami/postgresql
 resource "helm_release" "postgresql_dev" {
   count      = var.env == "prod" ? 0 : 1
   name       = "bitnami"
   repository = "https://charts.bitnami.com/bitnami"
   chart      = "postgresql"
 
+  values = [
+    "${file("${path.module}/values.yaml")}"
+  ]
+
   set_sensitive {
-    name  = "global.postgresql.postgresqlPassword"
+    name  = "global.postgresql.auth.postgresPassword"
     value = var.pgPass
   }
-
   set_sensitive {
-    name  = "global.postgresql.postgresqlUsername"
+    name  = "global.postgresql.auth.username"
     value = var.pgUser
   }
-
   set_sensitive {
-    name  = "global.postgresql.postgresqlDatabase"
+    name  = "global.postgresql.auth.password"
+    value = var.pgPass
+  }
+  set_sensitive {
+    name  = "global.postgresql.auth.database"
     value = var.pgDb
   }
-
 }
 
 // PGSQL PROD
@@ -30,17 +36,19 @@ resource "helm_release" "postgresql_prod" {
   chart      = "postgresql-ha"
 
   set_sensitive {
-    name  = "global.postgresql.password"
+    name  = "global.postgresql.auth.postgresPassword"
     value = var.pgPass
   }
-
   set_sensitive {
-    name  = "global.postgresql.username"
+    name  = "global.postgresql.auth.username"
     value = var.pgUser
   }
-
   set_sensitive {
-    name  = "global.postgresql.database"
+    name  = "global.postgresql.auth.password"
+    value = var.pgPass
+  }
+  set_sensitive {
+    name  = "global.postgresql.auth.database"
     value = var.pgDb
   }
 }
