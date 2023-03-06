7f27dd5b7aab11d94892014dc62f6b5952d7d261
Ramiro Castillo <rcastilloc@falabella.cl>
2023-03-06 20:31:54 -0300

    changed database ip on chart values

--- app/modules/flask-app/main.tf
+++ app/modules/flask-app/main.tf
@@ -1,5 +1,9 @@
 resource "helm_release" "flask-api" {
-  name       = "flask-api-helm_${var.env}"
+  name       = "flask-api-helm"
   repository = "file://./helm"
   chart      = "./helm"
+
+  values = [
+    "${file("./helm/values/${var.env}.yaml")}"
+  ]
 }
